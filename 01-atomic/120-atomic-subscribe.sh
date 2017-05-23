3844a6b28c4327bb59eb2111e971282351c0c104
dmitry sevostyanov <dmitry.sevostyanov@gmail.com>
2017-05-23 18:24:59 +0300

    work commit

--- 01-atomic/120-atomic-subscribe.sh
+++ 01-atomic/120-atomic-subscribe.sh
@@ -3,3 +3,9 @@
 . $(pwd)/env.sh || exit 1
 
 exec_cmd subscription-manager register --auto-attach --force
+exec_cmd subscription-manager repos \
+    --disable='*' \
+    --enable=rhel-7-server-rpms \
+    --enable=rhel-7-server-extras-rpms \
+    --enable=rhel-7-server-optional-rpms \
+    --enable=rhel-server-rhscl-7-rpms
